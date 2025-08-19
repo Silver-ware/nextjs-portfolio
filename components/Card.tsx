f86e8df018ed2f944f0e0be655fb9eedf1f5a417
Garry Caber <garrypedrosa.31@gmail.com>
2025-08-19 17:53:40 +0800

    Projects Section responsiveness

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Cards({project}: {project: ProjectInterface}) {
   return (
     <Card
-      className="flex flex-col w-[300px] items-center justify-center gap-3.5 px-4 py-5 bg-[#52525226] rounded-lg border border-solid border-neutral-700"
+      className="flex flex-col w-[300px] items-center justify-center md:justify-self-start justify-self-center gap-3.5 px-4 py-5 bg-[#52525226] rounded-lg border border-solid border-neutral-700"
     >
       <CardContent className="p-0 w-full">
         <div className="self-stretch relative h-36 rounded overflow-hidden">
@@ -17,7 +17,7 @@ export default function Cards({project}: {project: ProjectInterface}) {
         </div>
         <div className="flex flex-col items-start gap-1 mt-3.5 w-full">
           <div className="inline-flex items-start gap-2.5">
-            <h3 className="relative w-fit font-medium text-[#e2e2e2] text-xl tracking-[0] leading-[normal]">
+            <h3 className="relative w-fit font-medium text-[#e2e2e2] md:text-xl text-[18px] tracking-[0] leading-[normal]">
               {project.title}
             </h3>
           </div>
@@ -34,7 +34,7 @@ export default function Cards({project}: {project: ProjectInterface}) {
                 key={techIndex}
                 className="inline-flex items-center justify-center gap-2.5 px-3 py-1 bg-[#a3a3a31a] rounded-[5px]"
               >
-                <div className="relative w-fit font-normal text-[#e2e2e2] text-sm tracking-[0] leading-[normal]">
+                <div className="relative w-fit font-normal text-[#e2e2e2] md:text-sm text-xs tracking-[0] leading-[normal]">
                   {tech}
                 </div>
               </div>
--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -20,17 +20,24 @@ export default function Projects() {
         </h2>
       </div>
 
-      <div className="flex flex-wrap items-start gap-[24px_36px] px-0 py-2.5 w-full">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(270px,1fr))] justify-center gap-[24px_36px] px-0 py-2.5 w-full">
         {projectsData.map((project) => <Cards project={project} key={project.id}/>)}
       </div>
 
       {/* Pagination */}
-      <div className="flex items-start justify-between px-0 py-2.5 w-full">
+      <div className="flex w-full md:hidden items-center justify-end gap-1">
+            <input type="number" className="w-8 h-8 text-center p-1 bg-[#52525226] rounded-sm aspect-[1] relative font-medium text-[#e2e2e2] text-xs tracking-[0] leading-[normal] outline-0"/>
+            <div className="relative w-fit font-medium text-green-700 text-xs tracking-[0] leading-[normal]">
+              of 1
+            </div>
+          </div>
+
+      <div className="flex items-center justify-between px-0 py-2.5 w-full">
         <button
-          className="inline-flex items-center gap-1 px-3 py-2 bg-[#52525226] rounded-[5px]"
+          className="inline-flex items-center gap-1 md:px-3 px-2 py-2 bg-[#52525226] rounded-[5px]"
         >
-          <ChevronFirst className="w-6 h-6 text-[#e2e2e2]" />
-          <div className="relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
+          <ChevronFirst className="md:w-6 md:h-6 w-[18px] h-[18px] text-green-700" />
+          <div className="lg:inline hidden relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
             Go to the First List
           </div>
         </button>
@@ -39,8 +46,8 @@ export default function Projects() {
           <button
             className="inline-flex items-center gap-1 px-3 py-2 bg-[#52525226] rounded-[5px]"
           >
-            <ChevronLeft className="w-6 h-6 text-[#e2e2e2]" />
-            <div className="relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
+            <ChevronLeft className="md:w-6 md:h-6 w-[18px] h-[18px] text-green-700" />
+            <div className="relative w-fit font-medium text-[#e2e2e2] md:text-base text-xs tracking-[0] leading-[normal]">
               Prev
             </div>
           </button>
@@ -48,26 +55,26 @@ export default function Projects() {
           <button
             className="inline-flex items-center gap-1 px-3 py-2 bg-[#52525226] rounded-[5px]"
           >
-            <div className="relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
+            <div className="relative w-fit font-medium text-[#e2e2e2] md:text-base text-xs tracking-[0] leading-[normal]">
               Next
             </div>
-            <ChevronRight className="w-6 h-6 text-[#e2e2e2]" />
+            <ChevronRight className="md:w-6 md:h-6 w-[18px] h-[18px] text-green-700" />
           </button>
         </div>
 
         <div className="inline-flex items-center gap-4">
-          <div className="inline-flex items-center justify-center gap-1">
+          <div className="md:inline-flex hidden items-center justify-center gap-1">
             <input type="number" className="w-10 h-10 text-center p-2 bg-[#52525226] rounded-lg aspect-[1] relative font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal] outline-0"/>
             <div className="relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
               of 1
             </div>
           </div>
 
-          <button className="inline-flex items-center gap-1 px-3 py-2 bg-[#52525226] rounded-[5px]">
-            <div className="relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
+          <button className="inline-flex items-center gap-1 md:px-3 px-2 py-2 bg-[#52525226] rounded-[5px]">
+            <div className="lg:inline hidden relative w-fit font-medium text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
               Go to the Last List
             </div>
-            <ChevronLast className="w-6 h-6 text-[#e2e2e2]" />
+            <ChevronLast className="md:w-6 md:h-6 w-[18px] h-[18px] text-green-700" />
           </button>
         </div>
       </div>
