f8da82dbbf66393f06814b2b446074d123840bcb
Garry Caber <garrypedrosa.31@gmail.com>
2025-08-19 16:12:53 +0800

    Navbar and Hero Section responsiveness

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,33 +3,30 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="flex items-center justify-center gap-2.5 pt-[40px] pb-3 px-[110px] w-full">
-      <div className="flex flex-col basis-[765px] flex-1 h-[385px] items-start justify-center">
+    <section className="flex md:flex-row flex-col-reverse items-center justify-center gap-2.5 pt-[40px] pb-3 lg:px-[110px] md:px-[60px] px-6 w-full h-fit">
+      <div className="flex flex-col flex-1 h-fit md:items-start items-center justify-center">
         <div className="inline-flex items-center">
-          <h1 className="relative w-fit font-normal text-[#e2e2e2] text-4xl tracking-[0] leading-[30px] whitespace-nowrap">
-            <span className="[font-family:'Poppins-Regular',Helvetica] font-normal text-[#e2e2e2] text-4xl tracking-[0] leading-[30px]">
+          <p className="relative w-fit font-normal text-[#e2e2e2] tracking-[0] md:leading-[36px] text-wrap">
+            <span className="font-normal md:inline block lg:text-4xl md:text-3xl text-base text-center text-[#e2e2e2] tracking-[0] leading-[30px]">
               {"Hi, I'm "}
             </span>
-            <span className="font-extrabold">Garry Caber</span>
-            <span className="font-normal text-[#e2e2e2] text-4xl tracking-[0] leading-[30px]">
-              {" "}
-              & work like an{" "}
+            <span className="font-extrabold lg:text-4xl md:text-3xl text-2xl">Garry Caber</span>
+            <span className="font-normal text-[#e2e2e2] tracking-[0] leading-[30px] hidden lg:inline">
+              {" & work like an "}
             </span>
-            <span className="font-extrabold">ant</span>
-            <span className="font-normal text-[#e2e2e2] text-4xl tracking-[0] leading-[30px]">
-              .
-            </span>
-          </h1>
+            <span className="font-extrabold hidden lg:inline">ant</span>
+            <span className="font-normal md:inline hidden text-[#e2e2e2] tracking-[0] leading-[30px]">.</span>
+          </p>
         </div>
 
-        <div className="inline-flex items-center gap-2.5">
-          <h2 className="relative w-fit mt-[-1.00px] font-extrabold text-[#e2e2e2] text-5xl tracking-[0] leading-[normal]">
+        <div className="inline-flex md:py-2 items-center gap-2.5">
+          <h2 className="relative w-fit font-extrabold text-[#e2e2e2] lg:text-5xl md:text-[42px] text-[28px] tracking-[0] md:leading-[40px] leading-normal">
             Full Stack Developer
           </h2>
         </div>
 
         <div className="flex items-center gap-2.5 w-full">
-          <p className="relative w-fit flex-1 mt-[-1.00px] font-light italic text-[#e2e2e2] text-xl tracking-[0] leading-[normal]">
+          <p className="relative w-full text-wrap md:text-left text-center flex-1 font-light italic text-[#e2e2e2] lg:text-xl md:text-lg text-[14px] tracking-[0] leading-[normal]">
             Turning ideas into clean, efficient code—specializing in full-stack
             web development.
           </p>
@@ -37,21 +34,21 @@ export default function Hero() {
 
         <div className="inline-flex items-start gap-2.5 px-0 py-2.5">
           <button className="cursor-pointer inline-flex items-center justify-center px-3 py-2.5 bg-green-700 hover:bg-[#085D28] transition-all ease-in-out duration-500 rounded-[5px] border border-green-700 border-solid">
-            <span className="relative w-fit h-fit font-normal text-white text-base tracking-[0] leading-[normal]">
+            <span className="relative w-fit h-fit font-normal text-white lg:text-base text-[14px] tracking-[0] leading-[normal]">
               View Projects
             </span>
           </button>
 
           <button className="inline-flex items-center justify-center px-3 py-2.5 hover:bg-[#52525226]  transition-all ease-in-out duration-500 rounded-[5px] border border-solid border-neutral-700">
-            <span className="relative w-fit font-normal text-[#e2e2e2] text-base tracking-[0] leading-[normal]">
+            <span className="relative w-fit font-normal text-[#e2e2e2] lg:text-base text-[14px] tracking-[0] leading-[normal]">
               Contact Me
             </span>
           </button>
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center gap-2.5 pl-10 pr-0 py-10 flex-1 self-stretch grow">
-        <div className="relative w-[330px] h-[330px] rounded-full shadow-[0px_4px_95px_-15px_rgba(111,231,111,0.25)] border border-neutral-700 overflow-hidden">
+      <div className="flex flex-col items-center justify-center gap-2.5 md:pl-10 pr-0 md:py-10 py-3">
+        <div className="relative lg:w-[330px] lg:h-[330px] md:w-60 md:h-60 w-56 h-56 rounded-full shadow-[0px_4px_95px_-15px_rgba(111,231,111,0.25)] border border-neutral-700 overflow-hidden">
           <Image
             className="object-cover"
             src="/assets/images/profile.jpg"
