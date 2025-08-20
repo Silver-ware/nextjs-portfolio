c03f7454df17d920bd39113a9f4aedb223a41d02
Garry Caber <garrypedrosa.31@gmail.com>
2025-08-20 21:02:39 +0800

    Fix Scrollbar Behavior Issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import './globals.css';
-import { Poppins, Quantico } from 'next/font/google';
-import Navbar from '@/sections/Navbar';
+import "./globals.css";
+import { Poppins, Quantico } from "next/font/google";
+import Navbar from "@/sections/Navbar";
 
 export const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100','200','300','400','500','600','700','800','900'],
-  variable: '--font-poppins',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export const quantico = Quantico({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-quantico',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-quantico",
 });
 
 export default function RootLayout({
@@ -20,13 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} ${quantico.variable}`}>
+    <html
+      lang="en"
+      className={`${poppins.variable} ${quantico.variable}`}
+      style={{ scrollBehavior: "smooth" }}
+    >
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Garry Portfolio</title>
       </head>
-      <body className={`bg-[#1a1a1a] overflow-x-hidden antialiased`} cz-shortcut-listen="false">
-        <Navbar/>
+      <body
+        className={`bg-[#1a1a1a] overflow-x-hidden antialiased`}
+        cz-shortcut-listen="false"
+      >
+        <Navbar />
         {children}
       </body>
     </html>
